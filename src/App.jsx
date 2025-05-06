016b901fdef0d02b2339623ee65c8ec825b76f67
shaik sadhik <sadhikshaik9618@gmail.com>
2025-05-06 17:53:17 +0530

    changed nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import ListOfMovies from './pages/ListOfMovies';
